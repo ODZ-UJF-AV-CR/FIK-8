fb9660165e0d013b7e9fb582a730912583641372
Tedodor423 <60900662+Tedodor423@users.noreply.github.com>
2021-12-16 19:39:13 +0100

    Add files via upload

--- fw/CAMERA-ESP32/esp32cam_test/esp32cam_test.ino
+++ fw/CAMERA-ESP32/esp32cam_test/esp32cam_test.ino
@@ -1,18 +1,3 @@
-/*********
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-cam-take-photo-save-microsd-card
-  
-  IMPORTANT!!! 
-   - Select Board "AI Thinker ESP32-CAM"
-   - GPIO 0 must be connected to GND to upload a sketch
-   - After connecting GPIO 0 to GND, press the ESP32-CAM on-board RESET button to put your board in flashing mode
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*********/
-
 #include "esp_camera.h"
 #include "Arduino.h"
 #include "FS.h"                // SD Card ESP32
@@ -20,10 +5,6 @@
 #include "soc/soc.h"           // Disable brownour problems
 #include "soc/rtc_cntl_reg.h"  // Disable brownour problems
 #include "driver/rtc_io.h"
-#include <EEPROM.h>            // read and write from flash memory
-
-// define the number of bytes you want to access
-#define EEPROM_SIZE 1
 
 // Pin definition for CAMERA_MODEL_AI_THINKER
 #define PWDN_GPIO_NUM     32
@@ -44,15 +25,21 @@
 #define HREF_GPIO_NUM     23
 #define PCLK_GPIO_NUM     22
 
-int pictureNumber = 0;
+// settings
+uint8_t control_pin = 16;  // pin controling recording
+String image_folder_name = "/pictures";
+
+// variables
+uint32_t num, foldernum, picture_num, start_time=0, recording_start_time, previous_time;
+bool recording = false;
+String folder_name="";
 
 void setup() {
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
  
   Serial.begin(115200);
-  //Serial.setDebugOutput(true);
-  //Serial.println();
-  
+
+  // Camera config
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -74,79 +61,130 @@ void setup() {
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG; 
-  
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
-  } else {
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 12;
-    config.fb_count = 1;
-  }
-  
-  // Init Camera
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
-  }
-  
-  //Serial.println("Starting SD Card");
+
+  config.frame_size = FRAMESIZE_UXGA;  // QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+  config.jpeg_quality = 10;
+  config.fb_count = 1;
+
+  // Init SD Card
+  Serial.println("Initializing SD Card");
   if(!SD_MMC.begin()){
     Serial.println("SD Card Mount Failed");
     return;
   }
-  
   uint8_t cardType = SD_MMC.cardType();
   if(cardType == CARD_NONE){
     Serial.println("No SD Card attached");
     return;
   }
+
+  // Init Camera
+  Serial.println("Initializing Camera");
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  pinMode(control_pin, INPUT_PULLUP); // control pin
+}
+
+void loop() {
+  fs::FS &fs = SD_MMC;
+  if(!digitalRead(control_pin)){
+    if(recording){
+      capture(folder_name + "/image" + String(picture_num) + "_" +  String(millis() - start_time ) + ".jpg", fs);
+
+      // print info
+      Serial.print("Current FPS: ");
+      Serial.println(1000.0 / (millis() - previous_time));
+      previous_time = millis();
+      picture_num++;
     
+      Serial.print("Average FPS: ");
+      Serial.println(picture_num * 1000.0 / (millis() - recording_start_time));
+    }
+    else {  // start recording
+      if(!start_time){
+        start_time = millis();  // FIK-8 initialization time
+        }
+      
+      recording_start_time = previous_time = millis();
+      picture_num = 0;
+
+      num = get_last_folder_num(fs) + 1;
+      Serial.print("Starting recording number ");
+      Serial.println(num);
+
+      // create folder for recording
+      folder_name = image_folder_name + "/" + String(num);
+      if(!fs.mkdir(folder_name)){
+        Serial.println("Failed to create recording folder");
+      }
+
+      // log time of recording start
+      File millis_file = fs.open(folder_name + "/millis.txt", FILE_WRITE);
+      millis_file.print("start: " + String(millis() - start_time));
+      millis_file.close();
+
+      recording = true;
+      
+    }
+  }
+  else {
+   if(recording){
+    Serial.println("Ending recording...");
+    
+    // log time of recording end
+    File millis_file = fs.open(folder_name + "/millis.txt", FILE_APPEND);
+    millis_file.print("\nend: " + String(millis() - start_time));
+    millis_file.close();
+    
+    recording=false;
+    } 
+   delay(100);
+  }
+}
+
+void capture(String picture_name, fs::FS &fs){  
+  // take a picture
   camera_fb_t * fb = NULL;
-  
-  // Take Picture with Camera
   fb = esp_camera_fb_get();  
   if(!fb) {
     Serial.println("Camera capture failed");
     return;
   }
-  // initialize EEPROM with predefined size
-  EEPROM.begin(EEPROM_SIZE);
-  pictureNumber = EEPROM.read(0) + 1;
 
-  // Path where new picture will be saved in SD Card
-  String path = "/picture" + String(pictureNumber) +".jpg";
-
-  fs::FS &fs = SD_MMC; 
-  Serial.printf("Picture file name: %s\n", path.c_str());
+  // save the picture
+  File file = fs.open(picture_name.c_str(), FILE_WRITE);
   
-  File file = fs.open(path.c_str(), FILE_WRITE);
   if(!file){
     Serial.println("Failed to open file in writing mode");
-  } 
+  }
   else {
     file.write(fb->buf, fb->len); // payload (image), payload length
-    Serial.printf("Saved file to path: %s\n", path.c_str());
-    EEPROM.write(0, pictureNumber);
-    EEPROM.commit();
   }
   file.close();
-  esp_camera_fb_return(fb); 
   
-  // Turns off the ESP32-CAM white on-board LED (flash) connected to GPIO 4
-  pinMode(4, OUTPUT);
-  digitalWrite(4, LOW);
-  rtc_gpio_hold_en(GPIO_NUM_4);
-  
-  delay(2000);
-  Serial.println("Going to sleep now");
-  delay(2000);
-  esp_deep_sleep_start();
-  Serial.println("This will never be printed");
+  esp_camera_fb_return(fb);
 }
 
-void loop() {
+uint16_t get_last_folder_num(fs::FS &fs){
+  uint16_t num = 0;
+
+  // open directory
+  File dir = fs.open(image_folder_name);
+  File folder = dir.openNextFile();
   
+  while(folder){
+      if(folder.isDirectory()){
+          foldernum = String(folder.name()).substring(10).toInt();
+          if(foldernum > num){
+            num = foldernum;  // num will end up as the highest number in dir
+          }
+      }
+      folder = dir.openNextFile();
+  }
+  return num;
 }
